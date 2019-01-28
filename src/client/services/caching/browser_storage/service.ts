5c21c09f2d247502434edb16848647e445aab61b
Adam Yost <swimmadude66@gmail.com>
2019-01-27 21:13:27 -0500

    fix ssr

--- src/client/services/caching/browser_storage/service.ts
+++ src/client/services/caching/browser_storage/service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { TransferState, makeStateKey } from '@angular/platform-browser';
 import { StorageLocation } from '@models/shared/storage';
+import { ServerSideService } from '@services/ssr';
 
 @Injectable({
     providedIn: 'root'
@@ -9,53 +11,78 @@ export class BrowserStorageService {
     private _supportsLocal: boolean;
     private _supportsSession: boolean;
     private _inMem = {};
+    private _isServer: boolean;
 
     // determine if browser supports local and session storage, fallback to inMem
-    constructor() {
-        try {
-            localStorage.setItem('_bs_testLocal', 'stored');
-            const loc = localStorage.getItem('_bs_testLocal');
-            localStorage.removeItem('_bs_testLocal');
-            if (loc === 'stored') {
-                this._supportsLocal = true;
-            } else {
+    constructor(
+        private _transferState: TransferState,
+        private _ssr: ServerSideService
+    ) {
+        const isServer = this._ssr.isServer();
+        this._isServer = isServer;
+        if (isServer) {
+            this._supportsLocal = false;
+            this._supportsSession = false;
+        } else {
+            try {
+                localStorage.setItem('_bs_testLocal', 'stored');
+                const loc = localStorage.getItem('_bs_testLocal');
+                localStorage.removeItem('_bs_testLocal');
+                if (loc === 'stored') {
+                    this._supportsLocal = true;
+                } else {
+                    this._supportsLocal = false;
+                }
+            } catch(e) {
                 this._supportsLocal = false;
             }
-        } catch(e) {
-            this._supportsLocal = false;
-        }
-
-        try {
-            sessionStorage.setItem('_bs_testSession', 'stored');
-            const loc = sessionStorage.getItem('_bs_testSession');
-            sessionStorage.removeItem('_bs_testSession');
-            if (loc === 'stored') {
-                this._supportsSession = true;
-            } else {
+    
+            try {
+                sessionStorage.setItem('_bs_testSession', 'stored');
+                const loc = sessionStorage.getItem('_bs_testSession');
+                sessionStorage.removeItem('_bs_testSession');
+                if (loc === 'stored') {
+                    this._supportsSession = true;
+                } else {
+                    this._supportsSession = false;
+                }
+            } catch(e) {
                 this._supportsSession = false;
             }
-        } catch(e) {
-            this._supportsSession = false;
         }
     }
 
     setItem(key: string, value: string, locations: StorageLocation[] = ['local', 'session', 'memory']) {
-        for (let i=0; i < locations.length; i++) {
-            const loc = locations[i];
-            if (loc === 'local' && this._supportsLocal) {
-                this.setLocal(key, value);
-                break;
-            } else if (loc === 'session' && this._supportsSession) {
-                this.setSession(key, value);
-                break;
-            } else if (loc === 'memory') {
-                this.setInMem(key, value);
-                break;
+        if (this._isServer) {
+            const stateKey = makeStateKey(key);
+            this._transferState.set(stateKey, value);
+        } else {
+            for (let i=0; i < locations.length; i++) {
+                const loc = locations[i];
+                if (loc === 'local' && this._supportsLocal) {
+                    this.setLocal(key, value);
+                    break;
+                } else if (loc === 'session' && this._supportsSession) {
+                    this.setSession(key, value);
+                    break;
+                } else if (loc === 'memory') {
+                    this.setInMem(key, value);
+                    break;
+                }
             }
         }
     }
 
     getItem(key: string, locations: StorageLocation[] = ['local', 'session', 'memory']): string {
+        const stateKey = makeStateKey(key);
+        if (this._transferState.hasKey(stateKey)) {
+            const val = this._transferState.get(stateKey, null);
+            if (!this._isServer) {
+                this.setItem(key, val, locations);
+                this._transferState.remove(stateKey);
+            }
+            return val;
+        }
         for (let i=0; i < locations.length; i++) {
             const loc = locations[i];
             let val;
@@ -73,6 +100,10 @@ export class BrowserStorageService {
     }
 
     removeItem(key: string, locations: StorageLocation[] = ['local', 'session', 'memory']) {
+        const stateKey = makeStateKey(key);
+        if (this._transferState.hasKey(stateKey)) {
+            this._transferState.remove(stateKey);
+        }
         for (let i=0; i < locations.length; i++) {
             const loc = locations[i];
             if (loc === 'local' && this._supportsLocal) {
@@ -158,5 +189,4 @@ export class BrowserStorageService {
     clearInMem() {
         this._inMem = {};
     }
-
 }
